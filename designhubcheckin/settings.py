c482dac21ade3d466bf1980d46c4663c3e9457c8
Isakiye Afasha <afashaisakiye@gmail>
2020-10-31 13:56:50 +0300

    Fixed mime types on heroku

--- designhubcheckin/settings.py
+++ designhubcheckin/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 from django.contrib.messages import constants as messages
+import mimetypes
 
+mimetypes.add_type("text/css", ".css", True)
 MESSAGE_TAGS = {
     messages.ERROR: '',
     40: 'danger',
