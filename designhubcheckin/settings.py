4d929aa1bfcaadacf06bf5ac42fa93e0bb30b4c1
Isakiye Afasha <afashaisakiye@gmail>
2020-11-01 09:20:20 +0300

    Added white

--- designhubcheckin/settings.py
+++ designhubcheckin/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'designhubcheckin.urls'
@@ -133,3 +134,4 @@ STATIC_ROOT = Path.joinpath(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
